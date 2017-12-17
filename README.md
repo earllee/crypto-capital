5ddd004e94e512359928920c87319ceff662f711
Earl Lee <earlvlee@gmail.com>
2017-12-17 00:08:38 -0500

    :rocket: Lift off!

---
+++ README.md
@@ -0,0 +1,26 @@
+# gdax-cli
+A simple command-line tool to query GDAX for your current total account value and values (in USD) of each cryptoasset you own.
+
+GDAX is Coinbase's cryptoasset exchange.
+
+# Setup
+To run this, you will need to install the Node packages and set up authentication with GDAX.
+
+## Installing Node Packages
+1. In the home directory, run `yarn install`.
+  - Don't have Yarn? Visit https://yarnpkg.com/en/docs/install
+
+## Setting Up Authentication
+1. Create a GDAX API key with ONLY the "View" permission. You can create them here: https://www.gdax.com/settings/api.
+2. Copy the example auth file into `auth.json`, e.g. `cp auth-example.json auth.json`.
+3. Fill in in `auth.json` with your GDAX API credentials.
+
+# Running
+1. Run `node main.js`
+
+# Future Work
+- Streaming quotes and account values
+- Portfolio performance metrics
+
+# DISCLAIMERS
+> THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ main.js
@@ -0,0 +1,69 @@
+const gdax = require('gdax');
+
+// Load authentication words for private client
+const auth = require('./auth.json');
+const key = auth.API_KEY;
+const b64secret = auth.API_SECRET;
+const passphrase = auth.API_PHRASE;
+
+// Set up public clients for each cryptoasset
+const btcPublicClient = new gdax.PublicClient('BTC-USD');
+const ethPublicClient = new gdax.PublicClient('ETH-USD');
+const ltcPublicClient = new gdax.PublicClient('LTC-USD');
+
+// Set up private client
+const apiURI = 'https://api.gdax.com';
+const sandboxURI = 'https://api-public.sandbox.gdax.com';
+const authedClient = new gdax.AuthenticatedClient(key, b64secret, passphrase, apiURI);
+
+function toUSD(number) {
+  return parseFloat(number).toLocaleString('en-US', { 'style' : 'currency', 'currency' : 'USD' });
+}
+
+authedClient.getAccounts()
+  .then(data => {
+
+    console.log('Updated: ' + Date().toLocaleString());
+
+    var promiseData = data.map(account => {
+      if (account.currency === 'USD')
+        return { 'priceData' : { 'price' : '1.0' }, 'account' : account };
+
+      var publicClient = new gdax.PublicClient(account.currency + '-USD');
+      return publicClient.getProductTicker()
+        .then(data => {
+          return { 'priceData' : data, 'account' : account };
+        })
+        .catch(error => {
+          return error;
+        })
+    });
+
+    // Reorder USD data to be last
+    let usdData = promiseData[1];
+    promiseData.splice(1, 1).push(usdData);
+    promiseData.push(usdData);
+
+    // Run once GDAX pricing data for all cryptoassets are pulled
+    Promise.all(promiseData).then(datas => {
+
+      // Calculate total account value
+      let totalAccountValue = datas.reduce((prev, curr) => {
+        return parseFloat(curr.priceData.price * curr.account.balance) + prev;
+      }, 0.0);
+
+      // Print value for each cryptoasset
+      for (datum of datas) {
+        let value = parseFloat(datum.priceData.price * datum.account.balance);
+        let percent = (value / totalAccountValue).toLocaleString(undefined, { 'style' : 'percent', 'minimumFractionDigits' : 1 });
+        console.log(datum.account.currency + ': ' + toUSD(value) + ' (' + percent + ')');
+      }
+
+      // Print total account value
+      console.log('Total Account Value: ' + toUSD(totalAccountValue));
+    });
+
+  })
+  .catch(error => {
+    console.log(error);
+  });
